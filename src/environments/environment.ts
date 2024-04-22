4357f56487a502d82d30dbe73544bab4404defda
Andres <conocimineto.02@gmail.com>
2024-04-22 13:03:03 -0500

    nuevo

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://api-server-mu8i.onrender.com/api',
+  URL_SERVER: 'http://localhost:5000/api/v1',
+  URL_ADMIN: 'http://localhost:3000/api/usuarios',
+  URL_SERVICES: 'http://localhost:3000/api/usuarios',
+  URL_WS : 'http://localhost:5000',
+};
